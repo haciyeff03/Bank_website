1fe2278b4964f4660b65753dbe0035f14ddb4ff6
Qurban Haciyev <haciyevqurban97@gmail.com>
2025-02-07 23:57:38 +0400

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[20px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white mr-10`}>
+            className={`font-poppins font-normal cursor-pointer text-[20px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white `}>
             <a href={`#${nav.id}`}>
               {nav.title}
             </a>
@@ -22,7 +22,21 @@ const Navbar = () => {
       <div className="sm:hidden flex flex-1 justify-end items-center">
 <img src={toogle ? close : menu} alt="" 
 className="w-[28px] h-[28px] object-contain"
-onClick={()=> setToogle(!toogle)}/>
+onClick={()=> setToogle((prev)=>!prev )}/>
+<div className={`${toogle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+<ul className="list-none flex-col flex  justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white `}>
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+</div>
       </div>
     </nav>
   )
