a07a4e39fa0dad7b7fc1a1f630eaf6611778fe76
Qurban Haciyev <haciyevqurban97@gmail.com>
2025-04-17 01:33:02 +0400

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,26 +5,27 @@ const Navbar = () => {
 
   const [toogle,setToogle]=useState(false);
   return (
-    <nav className="w-full flex py-6 justify-between items-center navbar">
+    <nav className="flex items-center justify-between w-full py-6 navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+      <ul className="items-center justify-end flex-1 hidden list-none sm:flex">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
             className={`font-poppins font-normal cursor-pointer text-[20px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white `}>
             <a href={`#${nav.id}`}>
+              
               {nav.title}
             </a>
           </li>
         ))}
       </ul>
-      <div className="sm:hidden flex flex-1 justify-end items-center">
+      <div className="flex items-center justify-end flex-1 sm:hidden">
 <img src={toogle ? close : menu} alt="" 
 className="w-[28px] h-[28px] object-contain"
 onClick={()=> setToogle((prev)=>!prev )}/>
 <div className={`${toogle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
-<ul className="list-none flex-col flex  justify-end items-center flex-1">
+<ul className="flex flex-col items-center justify-end flex-1 list-none">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
